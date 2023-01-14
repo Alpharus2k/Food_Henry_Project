c66e3819626ae88f6c59d878bd653527278dc9cf
constantin_m <cmitrokhin@applaudostudios.dev>
2023-01-14 09:43:39 -0300

    Corregir errores de tipeo Diet.js

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('diet', {
     // Usa UUID como Primary Key
     id: {
       type: DataTypes.UUID,
