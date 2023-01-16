1f8d93f7532a5b9093e88e603dfcc72c14683c85
constantin_m <cmitrokhin@applaudostudios.dev>
2023-01-16 14:37:53 -0300

    Nada

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+//import { Route, useLocation } from "react-router-dom";
+//import NavBar from "./components/NavBar/Navbar";
+//import Home from "./components/Home/Home";
 
 function App() {
+
   return (
     <div className="App">
       <h1>Henry Food</h1>
