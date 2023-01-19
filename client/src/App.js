287d00908a4cff708d584e90b9852d3ce4de2888
constantin_m <cmitrokhin@applaudostudios.dev>
2023-01-19 20:49:58 -0300

    Impletentar prototipo de Recipe-Form de ruta /Create

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import PageNotFound from './components/PageNotFound/PageNotFound';
 import Home from './components/Home/Home';
 import Landing from './components/Landing/Landing';
-import FormRecipe from './components/Forms/Recipe-Form';
+import RecipeForm from './components/Forms/Recipe-Form';
 import RecipeFull from './components/Recipe/Recipe-Full';
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
           <Route exact path={"/"} element={<Landing/>}> </Route>
           <Route path="/home" element={<Home/>}></Route>
           <Route path="/recipe/:recipeId" element={<RecipeFull/>}></Route>
-          <Route path="/create" element={<FormRecipe/>}></Route>
+          <Route path="/create" element={<RecipeForm/>}></Route>
           <Route path="*" element={<PageNotFound/>}></Route>
       </Routes>
 
---
+++ client/src/components/Errors/Error-Recipe-Form.js
@@ -0,0 +1,7 @@
+export const NO_NAME    = "Se requiere un nombre";
+export const NO_DESC    = "Se requiere una descripción";
+export const NO_SCORE   = "El puntaje tiene que ser Numérico entre el 1 y 100";
+export const NO_STEPS   = "No se ingresaron pasos a seguir";
+export const NO_DIET    = "Indique al menos 1 dieta";
+
+
